b5793ff1a325c0af1e8510706c28625c9169fd60
2joopiter <veryberrylike3@gmail.com>
2023-11-20 10:58:45 +0900

    컴포넌트 설명

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 function App() {
 	return (
 		<div className='App'>
-			<h1>World</h1>
+			<h2>World</h2>
 		</div>
 	);
 }
 
 export default App;
+
+
+// 컴포넌트 함수의 필수조건
+// 1. 무조건 함수명을 대문자로 시작함
+// 2. 무조건 JSX를 리턴
+
+// JSX: 리액트에서 전용으로 쓰이는 돔 생성 문법
+// 원래는 React.createElement()라는 메서드를 이용해서 복잡하게 구성해야 되는 로직을 HTML 태그를 따라한 친화적인 문법으로 편하게 돔을 생성하게 만들어주는 리액트만의 문법 체계
