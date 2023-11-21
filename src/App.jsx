e8aea6228eb84df527cdf5214bef2c303cc281ed
2joopiter <veryberrylike3@gmail.com>
2023-11-21 12:45:26 +0900

    미션 결과 코드

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import Card from './card/Card';
 
 export default function App() {
 	const colors = ['coral', 'gold', 'lightpink', 'skyblue', 'plum', 'cornflowerblue'];
+	const widths = [250, 300, 500, 150, 200];
+
 	return (
 		<>
 			<h1>Color Chart</h1>
 			{colors.map((color, idx) => {
 				// 컴포넌트 호출시 특정값을 key={value} 형태로 컴포넌트 내부에 전달 가능(props)
-				return <Card key={color + idx} colorName={color} index={idx} />;
+				return <Card key={color + idx} colorName={color} index={idx} wid={200} />;
 			})}
+			<Card colorName={'black'} index={6} wid={400} show={'none'} />;
 		</>
 	);
 }
--- src/card/Card.jsx
+++ src/card/Card.jsx
@@ -1,9 +1,12 @@
 import './Card.scss';
 
+// 미션1 : 부모에서 해당 컴포넌트 호출시 props로 width 값을 전달받아서 각 카드 컴포넌트의 크기가 다르도록 호출
+// 미션2 - 부모의 특정 값에 따라 txtBox를 보임 유무를 결정할 수 있게 호출
+
 export default function Card(props) {
 	// 부모 컴포넌트에 전달되는 요소들을 props라는 객체로 묶어서 자식 컴포넌트의 파라미터 전달받음
 	return (
-		<article className='Card'>
+		<article className='Card' style={{ width: props.wid }}>
 			<div className='bgBox' style={{ backgroundColor: props.colorName }}></div>
 			<div className='txtBox'>
 				{props.index + 1}-{props.colorName}
