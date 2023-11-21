d134a5ce677c2931e30818ca626a142237c1d510
2joopiter <veryberrylike3@gmail.com>
2023-11-21 11:52:18 +0900

    map으로 특정 데이터를 반복돌리면서 동적으로 JSX 반환시 Key값이 필요한 이유

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import Box from './box/Box';
-
 export default function App() {
+	const colors = ['coral', 'gold', 'lightpink', 'skyblue', 'plum', 'cornflowerblue'];
 	return (
 		<>
-			<Box />
+			<h1>Color Chart</h1>
+
+			<ul>
+				{colors.map((color, idx) => {
+					// return문에서 map을 통해 반복적으로 생성이 되는 가상돔 요소에는 무조건 고유의 key값을 줘야 됨
+					// react로 하여금 여러개의 반복요소 중에서 수정요소를 편하게 비교탐색하게 하기 위함
+					// key에 집어넣을 값이 정말 없을 때는 반복되는 순서값을 집어넣을 수 있긴 하나 권장하진 않음
+					// DB의 각 데이터에 내장되어있는 object_id(객체 고유값)를 사용하거나 실제 데이터와 순번을 결합해서 고유 문자값 생성
+					// 지금의 경우 실제 데이터(color)+순번(idx)
+
+					return (
+						<li key={color + idx} style={{ color: color }}>
+							{color}
+						</li>
+					);
+				})}
+			</ul>
 		</>
 	);
 }
