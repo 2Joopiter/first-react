942d12c1d2ec75ecd283b35faade73fdfeea5bf0
2joopiter <veryberrylike3@gmail.com>
2023-11-22 13:14:49 +0900

    리액트에서 state값을 전위, 후위 증감연산자를 쓸 때의 예상치못한 에러상황->비추천

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,9 @@ export default function App() {
 
 	return (
 		<>
-			<button onClick={() => setIndex(--Index)}>left</button>
+			{/* 이벤트를 발생시켜서 setIndex가 실행이 되는 순간에 값이 0임 (해당값의 초기값이 먼저 활용됨 (setIndex(0)) */}
+			<button onClick={() => setIndex(Index--)}>left</button>
+			{/* 전위증가연산자라서 setIndex가 실행되는 순간에 바로 초기값을 먼저 증가시켜놓은 뒤 호출되므로 (setIndex(1)) */}
 			<button onClick={() => setIndex(++Index)}>right</button>
 			<div className='box' style={{ transform: `rotate(${45 * Index}deg)` }}></div>
 		</>
