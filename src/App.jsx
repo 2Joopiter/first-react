2ee4c44cb4d813b95641715dbe4fb22715b244e6
2joopiter <veryberrylike3@gmail.com>
2023-11-22 12:50:24 +0900

    state값 변경 이후 실제 반영되는 렌더링 사이클 순서

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,33 @@
-import Modal from './modal/Modal';
 import { useState } from 'react';
 
+/*
+
+	State가 변경 및 반영되는 시점
+	1. 이벤트 발생시 State 변경함수로 해당 값이 바뀔때 해당 렌더링 사이클에서 변경됨
+	2. 변경된 State값이 실제 DOM에 반영되는 사이클은 그 다음번 렌더링 사이클
+
+	=> 사이클이 돌 때 변경점을 체크하고 변경되지만, 반영은 첫 렌더링 사이클이 끝나고 다음 렌더링 사이클에 반영됨
+
+	(최초 생성된 돔->렌더 1회 실시) -> (변경점을 체크한 1번째 돔(변경점은 체크되나 최초 생성된 돔 상태를 출력)) -> (변경점을 반영한 2번째 돔(변경된 상태를 출력))
+	=> 아마도 리액트는 변경점을 객체로 묶어서 반영하기 때문에 끝까지 체크 된 후에 반영을 하는게 아닌가 싶음
+
+*/
+
 export default function App() {
-	const Colors = ['gold', 'cornflowerblue', 'plum', 'seagreen', 'crimson'];
-	const [IsOpen, setIsOpen] = useState(false);
+	const [Txt, setTxt] = useState('old');
 
 	/*
-	자주쓰는 패턴 (정보값을 토대로 반복 가상돔 생성)
-	1. return문 안쪽에서 {} 생성
-	2. 중괄호 안쪽에서 배열 state.map((반복 데이터, 순서))=>{
-		return
-	]
-	*/
-
-	const [DB, setDB] = useState(Colors);
-	// DB-화면에 동적으로 배열의 색상값을 토대로 반복 출력하기 위한 정보값. 해당 이름으로 버튼명을 생성하기 위한 정보값
-	const [Color, setColor] = useState('black');
-	// 이미 생성된 돔에 이벤트 발생시 해당 값을 변경하기 위한 State. 해당 버튼에 있는 문자값을 가져와서 실제 h1의 색상값을 변경하기 위한 정보값
+위의 로직 실행 순서
+1. State값 초기화 (render1)
+2. 버튼 클릭시 State값 변경은 되지만 실제 반영되진 않음 (render1)
+3. 리액트가 내부적으로 state값 변경을 인지해서 컴포넌트 재 실행
+4. 이전 렌더링 사이클에서 변경된 state값이 실제 반영됨 (render2)
+*/
 
 	return (
 		<>
-			{DB.map((data, idx) => {
-				return (
-					<button key={data + idx} onClick={() => setColor(data)}>
-						{/* 여기서 onclick이 없으면 Color State는 없어도 됨. 하지만 색상 변경이 필요하기 때문에 Color State가 추가됨*/}
-						{data}
-					</button>
-				);
-			})}
-
-			<h1 style={{ color: Color }}>부모 컴포넌트</h1>
-
-			<button onClick={() => setIsOpen(!IsOpen)}>{IsOpen ? '팝업 닫기' : '팝업 열기'}</button>
-			{IsOpen && <Modal />}
+			<h1>{Txt}</h1>
+			<button onClick={() => setTxt('new')}>글자변경</button>
 		</>
 	);
 }
