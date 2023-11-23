1b91c817168a7a484ba22ebd8dbae4b6db62f71f
2joopiter <veryberrylike3@gmail.com>
2023-11-23 10:18:14 +0900

    useEffect의 실사용사례

--- src/modal/Modal.jsx
+++ src/modal/Modal.jsx
@@ -33,10 +33,16 @@ export default function Modal({ setOpen }) {
   useEffect((콜백함수)=>{실행할 구문},[의존성배열])
   -컴포넌트 생성(Mount)
     : useEffect에 의존성 배열이 비어있는 상태에서 입력한 콜백함수는 컴포넌트 생성시 한번만 실행됨
+    : 실사례1- 모달창 생성시 강제로 스크롤바 제거 해야될 때
+    : 실사례2- resize, scroll, mousewheel, mousemove같은 window 전역에 직접 이벤트 연결해야되는 특정 컴포넌트 전용함수를 등록할 때
+    : 실사례3(중요)- data fetching같이 반복 호출할 필요가 없는 무거운 연산을 처리할 때
 
   -컴포넌트 변경(State Change)
     : useEffect 의존성 배열에 특정 state를 등록하면 해당 state가 변경될 때마다 실행됨
 
   -컴포넌트 소멸(Unmount)
     : useEffect에 의존성 배열이 비어있는 상태에서 리턴함수를 내보내면 컴포넌트 소멸시 한번만 실행됨 
+    : 실사례1- 모달창 제거시 다시 스크롤바 활성화 시킬 때
+    : 실사례2-window 전역에 연결되어있는 특정 컴포넌트 함수를 제거할 때
+    : 
 */
